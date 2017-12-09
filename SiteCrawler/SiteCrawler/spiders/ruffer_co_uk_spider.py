d6b1d2f9521a662789d62a8e9efb63729110ebce
0x3F3F <26686669+0x3F3F@users.noreply.github.com>
2017-12-09 14:46:02 +0100

    UpdateSpider to only get Ruffer Comment

--- SiteCrawler/SiteCrawler/spiders/ruffer_co_uk_spider.py
+++ SiteCrawler/SiteCrawler/spiders/ruffer_co_uk_spider.py
@@ -1,44 +1,75 @@
 import scrapy
 import datetime, time
+from collections import namedtuple
 
-# TODO: Add in processing for Annual Reports
+# Structure to store our css in
+CssItems = namedtuple('CssItems', ['LoopCss', 'TitleCss', 'LinkCss'])
+
 class RufferSpider(scrapy.Spider):
 	name = "Ruffer"
 	allowed_domains = ['https://www.ruffer.co.uk']
-	start_urls = ['https://www.ruffer.co.uk/about/investment-review', ]
+	start_urls = [ ] # setup later
+
+	# Store the Css elements we want for each url.  
+	# EXAMPLE MORE COMPLICATED AS WAS GOING TO FETCH ITEMS FROM MUILTIPLE PAGES, but in end not.
+	# HAck: Using http/https as 2 areas of that page we and to scrape, 2 dict entries.
+	urlItemsDict = {}
+	urlItemsDict['https://www.ruffer.co.uk/about/investment-review'] = CssItems('article.review','h2::text','a::attr("href")')	# Ruffer Coment
+	#urlItemsDict['https://www.ruffer.co.uk/funds/ruffer-investment-company'] = CssItems('section.not-white','a::text','a::attr("href")') # Annual Report USE AIC INSTEAD
+	#urlItemsDict['http://www.ruffer.co.uk/funds/ruffer-investment-company'] = CssItems('div.filters ul','li button::text','li button::attr("data-value")') # Monthly Reaport USE AIC INSTEAD
 
 	# Custom settings that I can read in the pipeline & put in feed.
 	custom_settings = {}
 	custom_settings['RSS_TITLE'] = 'Ruffer Investment Trust'
-	custom_settings['RSS_LINK'] = start_urls[0]
+	custom_settings['RSS_LINK'] = 'http://www.ruffer.co.uk'
 	custom_settings['RSS_OUTPUT_FILE'] = name + '.rss'
  
-	def getPubDate(self):
-		"""Creates a RSS date/time"""
-		pubDate = datetime.date.today().strftime("%d %B %Y")
-		pubTime = time.strftime('%H:%M')
-		return(pubDate + " " +  pubTime)
+
+	def __init__(self, name=None, **kwargs):
+
+		# scrapy wants start_urls.  To avoid duplication, I'll populate here
+		self.start_urls = list(self.urlItemsDict.keys()) 
+
+		# Call original Init function.  Dont need to callm as just sets name and start_url
+		#scrapy.Spider.__init__(self, name, kwargs)
+
+
+	def GetCleanTitleLink(self, extractedTitle, extractedLink ):
+		"""Checks titl/link for scrape error (None).  Adds domain to link. """
+
+		if not extractedTitle:
+			title = "RICA: Problem Extracting h2 text (title)"
+		else:
+			title = extractedTitle
+
+		if not extractedLink:
+			link = "Problem Extracting Link Href"
+		else:
+			# It's a relative link, so add the domain back in
+			link = self.allowed_domains[0] + "/" + extractedLink
+
+		return title, link
 
 
 	def parse(self, response):
 		"""Select page elements to pick for the generated xml element"""
 
-		for viewRow in response.css('article.review'):
+		# Get the appropriate selectors, depending on the page we're scraping
+		itemsSelector = self.urlItemsDict[response.url].LoopCss 
+		titleSelector = self.urlItemsDict[response.url].TitleCss 
+		linkSelector = self.urlItemsDict[response.url].LinkCss 
+
+		for viewRow in response.css(itemsSelector):
 
 			# I've set the order here in FEED_EXPORT_FIELDS cfg variable
-			title = viewRow.css('h2::text').extract_first()		
-			link = self.allowed_domains[0]+"/"+viewRow.css('a::attr("href")').extract_first()
-			pubDateTime = self.getPubDate()
+			extractedTitle = viewRow.css(titleSelector).extract_first()		
+			extractedLink = viewRow.css(linkSelector).extract_first()
+
+			title, link = self.GetCleanTitleLink(extractedTitle, extractedLink )
 
 			yield {
 			'title': title,
 			'link': link,
 			'guid' : link,
-			'pubDate' : pubDateTime,
 			'description': self.name  + " " + title,
 			}
-		
-		# Don't really casre about next page, but if I did then this might work:	
-		#next_page = response.css('li.pager-next a::attr("href")').extract_first()
-		#if next_page is not None:
-		#	yield response.follow(next_page, self.parse)
