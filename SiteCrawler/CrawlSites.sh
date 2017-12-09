ef491d77c5c452f1319b326a0551f396ddc591c3
0x3F3F <26686669+0x3F3F@users.noreply.github.com>
2017-12-09 16:54:08 +0100

    Update script to inlcude ITs

---
+++ SiteCrawler/CrawlSites.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# cd into SiteCrawler project dir
+cd /home/pi/Dev/RssTools/SiteCrawler
+
+#################################################
+# Detail Spiders and Status. ALPHABETICAL ORDER !
+#################################################
+
+# Spider: british-empire_co_uk_spider.py
+# Output: BritishEmpire.rss
+# Notes: Not used as 
+#scrapy crawl BritishEmpire
+
+# Spider: fincialexpressde_net_spider.py
+# Output: InvestmentTrustReps.rss
+# Notes: Main Spider that fetches all IT Reports from theaic.so.uk data provider
+scrapy crawl InvestmentTrustReps
+
+# Spider: hansatrust_com_spider.py
+# Output: HansaTrust.rss
+# Notes: Not used as using financialexpress_net_spider.py instead 
+#scrapy crawl HansaTrust
+
+# Spider: patplc_co_uk_spider.py
+# Output: PersonalAssets.rss
+# Notes: Not used as using financialexpress_net_spider.py instead 
+#scrapy crawl PersonalAssets
+
+# Spider: ruffer_co_uk_spider.py
+# Output: Ruffer.rss
+# Notes:Only used for Ruffer Comment.  Monthly/Annual reprots from finanicalExpress_new_spider.py
+scrapy crawl Ruffer
+
+#########################################################################
+# Note:
+# Setup symlinks for desired feeds from web dir to feed output directory
+# Can then just add the feesd into local feedreader, pointing to localhost
+#
+# Run this on a cron
+##########################################################################
+
+
+
